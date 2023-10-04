a639803b74ebfe384ae215e11317d138d2faeacc
RubyChou <ruby.chou@gmail.com>
2023-10-04 20:43:15 +0800

    Add 2_printf for type int,char, char [];

---
+++ 1_basic_example/2_printf.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+void main(void)
+{
+    int tea_price = 35;
+    int num = 5;
+
+    char a='a';
+    char string[] = "12345\0";
+
+	
+    printf("tea_price:%d, num:%d, total=%d\n",
+	tea_price,num,total);
+
+    printf("a=%c, a=%d\n",a,a);	
+
+    printf("string:%s, string[0]=%d=%d\n",
+	string, string[0],string[0]);
+}
