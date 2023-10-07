f7daa3bc54a8dd783479a095f1c1e9e8608e6201
ruby.chou <ruby.chou@gamil.com>
2023-10-07 15:37:50 +0800

    Add example for python data type

---
+++ 2_Variables_arithmetic/data.py
@@ -0,0 +1,13 @@
+# python version
+
+def main():
+    var_char='c'
+    var_int=10
+    var_long=18446744073709551615
+
+    print(type(var_char))
+    print(type(var_int))
+    print(type(var_long))
+
+if __name__ == "__main__":
+  main()
