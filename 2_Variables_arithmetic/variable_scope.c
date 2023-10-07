965921f09ef6dc6fed07116c24da6469596ffd95
RubyChou <ruby.chou@gmail.com>
2023-10-07 19:27:55 +0800

    Print address in variable_scope.c

--- 2_Variables_arithmetic/variable_scope.c
+++ 2_Variables_arithmetic/variable_scope.c
@@ -12,7 +12,7 @@ long * var_long_pointer;
 
 void test_print(void)
 {
-    printf("[%s:%d] var_int:%d\n",__func__,__LINE__,var_int);
+    printf("[%s:%d] sizeof(var_int)=%d,&var_int=0x%x\n",__func__,__LINE__,sizeof(var_int),&var_int);
 }
 
 
@@ -20,9 +20,9 @@ int main(void)
 {
     int var_int=1;
 
-    printf("sizeof(var_char)=%d\n",sizeof(var_char));
-    printf("sizeof(var_int)=%d\n",sizeof(var_int));
-    printf("sizeof(var_long)=%d\n",sizeof(var_long));
+    printf("sizeof(var_char)=%d,&var_char=0x%x\n",sizeof(var_char),&var_char);
+    printf("[%s:%d] sizeof(var_int)=%d,&var_int=0x%x\n",__func__,__LINE__,sizeof(var_int),&var_int);
+    printf("sizeof(var_long)=%d,&var_long=0x%x\n",sizeof(var_long),&var_long);
     printf("sizeof(var_float)=%d\n",sizeof(var_float));
     printf("sizeof(var_char_pointer)=%d\n",sizeof(var_char_pointer));
     printf("sizeof(var_int_pointer)=%d\n",sizeof(var_int_pointer));
@@ -32,14 +32,14 @@ int main(void)
 
     {
         int var_int=2;
-        printf("[%s:%d] var_int:%d\n",__func__,__LINE__,var_int);
+    	printf("[%s:%d] sizeof(var_int)=%d,&var_int=0x%x\n",__func__,__LINE__,sizeof(var_int),&var_int);
     }
 
     {
         var_int=3;
     }
 
-    printf("[%s:%d] var_int:%d\n",__func__,__LINE__,var_int);
+    printf("[%s:%d] sizeof(var_int)=%d,&var_int=0x%x\n",__func__,__LINE__,sizeof(var_int),&var_int);
     test_print();
 
     exit(EXIT_SUCCESS);
