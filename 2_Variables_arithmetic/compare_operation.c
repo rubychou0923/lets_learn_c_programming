3f01778b3186853333c7af271e9f9a5ac60fb4da
ruby.chou <ruby.chou@gamil.com>
2023-10-07 18:36:21 +0800

    Add some operation compare between python and c.

---
+++ 2_Variables_arithmetic/compare_operation.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(void)
+{
+    int a=1;
+    int b=0;
+
+    if(a == b)
+        printf("a==b\n");
+
+    if(a !=b )
+        printf("a!=b\n");
+
+    if( a<=b )
+        printf("a<=b\n");    
+
+    if(1 && 0)
+        printf("1 && 0\n");
+
+    if(1 || 0)
+        printf("1 || 0\n");
+
+}
---
+++ 2_Variables_arithmetic/compare_operation.py
@@ -0,0 +1,23 @@
+# python version
+
+def main():
+    a=1
+    b=0
+
+    if(a == b):
+        print("a==b")
+
+    if(a != b ):
+        print("a!=b")
+
+    if( a<=b ):
+        print("a<=b")    
+
+    if(1 and 0):
+        print("1 and 0")
+
+    if(1 or 0):
+        print("1 or 0")
+
+if __name__ == "__main__":
+  main()
