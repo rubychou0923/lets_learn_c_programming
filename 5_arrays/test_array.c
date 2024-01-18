8f0603bcdaa94fc2f9df299aa8e3a585815697f2
Ruby Chou <ruby.chou@gmail.com>
2024-01-18 20:29:32 +0800

    modify test_array.c to more simpler

--- 5_arrays/test_array.c
+++ 5_arrays/test_array.c
@@ -3,15 +3,14 @@ void main(void)
 {
 
     int ar[10];
-    int *ip;
-    int (*ar10i)[10];       /* pointer to array of 10 ints */
 
-    ip = ar;                /* address of first element */
 
-    printf("%x \n",ip);
-    ip = &ar[0];            /* address of first element */
-    printf("%x \n",ip);
-    ar10i = &ar;            /* address of whole array */
-    printf("%x \n",ar10i);
+    printf("ar:0x%x \n",ar);
+
+    /* address of first element */
+    printf("&ar[0]=0x%x \n",&ar[0]);
+
+    /* address of whole array */
+    printf("&ar=0x%x \n",&ar);
 
 }
